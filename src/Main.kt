dadcd88df30ed5979a44abbfe1f879d47034753b
waimea-jestevenson <jestevenson@waimea.school.nz>
2025-02-24 10:59:21 +1300

    working on getting the calculator to calculate the equation

--- src/Main.kt
+++ src/Main.kt
@@ -1,42 +1,34 @@
 fun main() {
-var operation =
-    println("Welcome to the ultimate calculator 1000")
 
-getDouble("Enter the 1st number:")
-getString("Operation: + - * /:")
-getDouble("Enter the 2nd number:")
+    println("Welcome to the ultimate calculator 9000")
+
+val num1= getDouble("Enter the 1st number:")
+var operation= getOperation("Operation: + - * /:")
+val num2=getDouble("Enter the 2nd number:")
 
 when {
+    operation == ('+') -> (num1+num2)
+    operation == ('-') -> (num1-num2)
+    operation == ('*') -> (num1*num2)
+    operation == ('/') -> (num1/num2)
+
+    /**
+     * operation = + -> (first number + second number)
+     * repeat for each operation
+     */
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
+fun calculate(num1: Double, num2: Double): Double {
+    var result= 0.0
+    when {
+         ('+') -> result = (num1+num2)
+         ('-') -> (num1-num2)
+          ('*') -> (num1*num2)
+          ('/') -> (num1/num2)
+}
 
 
 /**
@@ -57,18 +49,31 @@ fun getString(prompt:String):String {
 
 /**
 function to get int from user**/
-fun getDouble(prompt:String):Int {
-    var userNumber: Int?
+fun getDouble(prompt:String):Double {
+    var userNumber: Double?
 
     // Keep looping until the user types something
     while (true) {
         // Get some text from the user
         val textInput = getString(prompt)
         // Try to convert to a number
-        userNumber = textInput.toIntOrNull()
+        userNumber = textInput.toDoubleOrNull()
         // Only leave loop if valid conversion
         if (userNumber != null) break
     }
     return userNumber!!
+}
+
+fun getOperation(prompt: String): Char {
+    var input: String?
+
+    // Keep looping until the user types something
+    while (true) {
+        input = getString(prompt)
+        if ("+-*/".contains(input))
+            break
+    }
+
+    return input.first()
 }
 
